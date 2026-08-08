ef2212a97151185f398f46450cfcba0735afea20
James <jamesvanmason923@gmail.com>
2026-08-08 15:57:56 -0500

    tentative asymmetric yaw servo fix, updated values in config.h

--- Arduino_Code_TVC26/controller.cpp
+++ Arduino_Code_TVC26/controller.cpp
@@ -15,6 +15,14 @@ led(ledObj)
     prevTime = 0;
 }
 
+float Controller::calculateYawCommand(float yawCommand) {
+    if(yawCommand >= 0.0) {
+        return YAW_CENTER + (yawCommand * YAW_POS_RATIO);
+    } else {
+        return YAW_CENTER + (yawCommand * YAW_NEG_RATIO);
+    }
+}
+
 void Controller::beginController() {
     //start leds first
     led.beginLEDs();
@@ -70,15 +78,15 @@ void Controller::updateController() {
     }
 
     //TVC Control:
-   switch(phase) {
+    switch(phase) {
     case POWERED:
     case COAST:
     {
-        //new targets need to be the pitch and yaw servo centers?
-        float pitchCommand = pitchPID.calculate(PITCH_CENTER, currentData.gyroY, dt);
-        float yawCommand = yawPID.calculate(YAW_CENTER, currentData.gyroZ, dt);
-        servos.setPitch(pitchCommand);
-        servos.setYaw(yawCommand);
+        //target represents ang velocity, so target = 0
+        float pitchCommand = pitchPID.calculate(0.0, currentData.gyroY, dt);
+        float yawCommand = yawPID.calculate(0.0, currentData.gyroZ, dt);
+        servos.setPitch(pitchCommand * PTICH_RATIO);
+        servos.setYaw(calculateYawCommand(yawCommand));
         break;
     }
     case GROUND:
